a659892cefbf3c9226fe5d8868ee384504d0a717
unknown <089.lukas@goooglemail.com>
2013-12-16 16:09:38 +0100

    new ZH translation

--- translations/unclient_zh.ts
+++ translations/unclient_zh.ts
@@ -1,35 +1,45 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="zh_CN">
+<TS version="2.0" language="zh">
 <context>
     <name>DialogUpdate</name>
     <message>
         <location filename="../multiappdialog.ui" line="14"/>
         <source>Software Update Manager</source>
-        <translation type="unfinished">Èí¼þ¸üÐÂ¹ÜÀíÆ÷</translation>
+        <translation type="unfinished">软件更新管理器</translation>
     </message>
     <message>
         <location filename="../multiappdialog.ui" line="91"/>
         <source>Close</source>
-        <translation type="unfinished">¹Ø±Õ</translation>
+        <translation type="unfinished">关闭</translation>
     </message>
     <message>
         <location filename="../multiappdialog.ui" line="107"/>
         <source>Check</source>
-        <translation type="unfinished">¼ì²é</translation>
+        <translation type="unfinished">检查</translation>
     </message>
     <message>
         <location filename="../multiappdialog.ui" line="140"/>
         <source>Install</source>
-        <translation type="unfinished">°²×°</translation>
+        <translation type="unfinished">安装</translation>
+    </message>
+    <message>
+        <location filename="../multiappdialog.ui" line="169"/>
+        <source>X</source>
+        <translation type="unfinished">X</translation>
     </message>
 </context>
 <context>
     <name>Helpdialog</name>
     <message>
+        <location filename="../helpdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">对话框</translation>
+    </message>
+    <message>
         <location filename="../helpdialog.ui" line="42"/>
         <source>Close</source>
-        <translation type="unfinished">¹Ø±Õ</translation>
+        <translation type="unfinished">关闭</translation>
     </message>
 </context>
 <context>
@@ -37,67 +47,67 @@
     <message>
         <location filename="../multiappdialog.cpp" line="134"/>
         <source> - Update Manager</source>
-        <translation type="unfinished">- ¸üÐÂ¹ÜÀíÆ÷</translation>
+        <translation type="unfinished">- 更新管理器</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="169"/>
         <source>Software Update Manager</source>
-        <translation type="unfinished">Èí¼þ¸üÐÂ¹ÜÀíÆ÷</translation>
+        <translation type="unfinished">软件更新管理器</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="234"/>
         <source> (Size: %1)</source>
-        <translation type="unfinished"> (´óÐ¡: %1)</translation>
+        <translation type="unfinished"> (大小: %1)</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="259"/>
         <source>There is a new software update available</source>
-        <translation type="unfinished">ÓÐÒ»¸öÐÂµÄÈí¼þ¿É¸üÐÂ</translation>
+        <translation type="unfinished">有一个新的软件可更新</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="261"/>
         <source>There are %1 new software updates available</source>
-        <translation type="unfinished">ÐÂµÄÈí¼þ»¹ÓÐ%1 ¸üÐÂÍê³É</translation>
+        <translation type="unfinished">新的软件还有%1 更新完成</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="263"/>
         <source>Your software is up to date</source>
-        <translation type="unfinished">ÄãµÄÈí¼þÊÇ×îÐÂµÄ</translation>
+        <translation type="unfinished">你的软件是最新的</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="309"/>
         <source>Downloading update %1 ...</source>
-        <translation type="unfinished">ÏÂÔØ¸üÐÂ %1 ...</translation>
+        <translation type="unfinished">下载更新 %1 ...</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="319"/>
         <source>All updates have been installed successfully</source>
-        <translation type="unfinished">ËùÓÐ¸üÐÂÒÑ¾­°²×°³É¹¦</translation>
+        <translation type="unfinished">所有更新已经安装成功</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="361"/>
         <source>Installing update &quot;%1&quot;</source>
-        <translation type="unfinished">°²×°¸üÐÂ &quot;%1&quot;</translation>
+        <translation type="unfinished">安装更新 &quot;%1&quot;</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="367"/>
         <source>Update failed:&lt;br&gt;%1</source>
-        <translation type="unfinished">¸üÐÂÊ§°Ü:&lt;br&gt;%1</translation>
+        <translation type="unfinished">更新失败:&lt;br&gt;%1</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="367"/>
         <source>Unable to execute the command!</source>
-        <translation type="unfinished">ÎÞ·¨Ö´ÐÐÃüÁî!</translation>
+        <translation type="unfinished">无法执行命令!</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="398"/>
         <source>Update &apos;%1&apos; installed successfully</source>
-        <translation type="unfinished">¸üÐÂ &apos;%1&apos; °²×°³É¹¦</translation>
+        <translation type="unfinished">更新 &apos;%1&apos; 安装成功</translation>
     </message>
     <message>
         <location filename="../multiappdialog.cpp" line="418"/>
         <source>Update &apos;%1&apos; failed with error %2</source>
-        <translation type="unfinished">¸üÐÂ&apos;%1&apos; ´íÎóÊ§°Ü%2</translation>
+        <translation type="unfinished">更新&apos;%1&apos; 错误失败%2</translation>
     </message>
 </context>
 <context>
@@ -105,68 +115,72 @@
     <message>
         <location filename="../main.cpp" line="116"/>
         <source>Unable to launch &apos;%1&apos;</source>
-        <translation type="unfinished">ÎÞ·¨Æô¶¯&apos;%1&apos;</translation>
+        <translation type="unfinished">无法启动&apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="334"/>
         <source>Checking for messages ...</source>
-        <translation type="unfinished">¼ì²éÐÅÏ¢ ...</translation>
+        <translation type="unfinished">检查信息 ...</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="336"/>
         <source>Checking for updates ...</source>
-        <translation type="unfinished">¼ì²é¸üÐÂ ...</translation>
+        <translation type="unfinished">检查更新 ...</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="44"/>
+        <source>%1 %2</source>
+        <translation type="unfinished">%1 %2</translation>
     </message>
     <message>
         <location filename="../systemtray.cpp" line="53"/>
         <source>Launch Update Client</source>
-        <translation type="unfinished">Æô¶¯¸üÐÂ¿Í»§¶Ë</translation>
+        <translation type="unfinished">启动更新客户端</translation>
     </message>
     <message>
         <location filename="../systemtray.cpp" line="58"/>
         <source>Read Messages</source>
-        <translation type="unfinished">¶ÁÈ¡ÐÅÏ¢</translation>
+        <translation type="unfinished">读取信息</translation>
     </message>
     <message>
-        <location filename="../singleappdialog.cpp" line="117"/>
         <location filename="../systemtray.cpp" line="62"/>
         <source>Close</source>
-        <translation type="unfinished">¹Ø±Õ</translation>
+        <translation type="unfinished">关闭</translation>
     </message>
     <message>
         <location filename="../updatenode_service.cpp" line="245"/>
         <location filename="../updatenode_service.cpp" line="269"/>
         <source>There are no new updates &amp; messages available</source>
-        <translation type="unfinished">Ã»ÓÐÐÂµÄ¸üÐÂºÍÐÅÏ¢</translation>
+        <translation type="unfinished">没有新的更新和信息</translation>
     </message>
     <message>
         <location filename="../updatenode_service.cpp" line="248"/>
         <location filename="../updatenode_service.cpp" line="272"/>
         <source>There are new updates available</source>
-        <translation type="unfinished">ÓÐÐÂµÄ¸üÐÂ¿ÉÓÃ</translation>
+        <translation type="unfinished">有新的更新可用</translation>
     </message>
     <message>
         <location filename="../updatenode_service.cpp" line="251"/>
         <location filename="../updatenode_service.cpp" line="275"/>
         <source>There are new messages available</source>
-        <translation type="unfinished">ÓÐÐÂÏûÏ¢</translation>
+        <translation type="unfinished">有新消息</translation>
     </message>
     <message>
         <location filename="../updatenode_service.cpp" line="254"/>
         <location filename="../updatenode_service.cpp" line="278"/>
         <source>There are updates and messages available</source>
-        <translation type="unfinished">ÓÐ¿ÉÓÃµÄ¸üÐÂºÍÐÅÏ¢</translation>
+        <translation type="unfinished">有可用的更新和信息</translation>
     </message>
     <message>
         <location filename="../updatenode_service.cpp" line="257"/>
         <location filename="../updatenode_service.cpp" line="281"/>
         <source>Undefined state</source>
-        <translation type="unfinished">Î´¶¨ÒåµÄ×´Ì¬</translation>
+        <translation type="unfinished">未定义的状态</translation>
     </message>
     <message>
         <location filename="../maccommander.cpp" line="20"/>
         <source> wants to make changes.</source>
-        <translation type="unfinished">ÏëÒª¸Ä±ä</translation>
+        <translation type="unfinished">想要改变</translation>
     </message>
 </context>
 <context>
@@ -174,52 +188,57 @@
     <message>
         <location filename="../singleappdialog.ui" line="28"/>
         <source>Initalizing ...</source>
-        <translation type="unfinished">³õÊ¼»¯ ...</translation>
+        <translation type="unfinished">初始化 ...</translation>
     </message>
     <message>
         <location filename="../singleappdialog.ui" line="58"/>
         <source>Show details</source>
-        <translation type="unfinished">ÏÔÊ¾ÏêÏ¸ÐÅÏ¢</translation>
+        <translation type="unfinished">显示详细信息</translation>
     </message>
     <message>
         <location filename="../singleappdialog.ui" line="71"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished">&quot;È¡Ïû&quot;</translation>
+        <translation type="unfinished">&quot;取消&quot;</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="82"/>
         <source>Downloading update ...</source>
-        <translation type="unfinished">ÏÂÔØ¸üÐÂ ...</translation>
+        <translation type="unfinished">下载更新 ...</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="84"/>
         <source>Downloading updates</source>
-        <translation type="unfinished">ÏÂÔØ¸üÐÂ</translation>
+        <translation type="unfinished">下载更新</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="100"/>
         <source>Installing update ...</source>
-        <translation type="unfinished">°²×°¸üÐÂ ...</translation>
+        <translation type="unfinished">安装更新 ...</translation>
+    </message>
+    <message>
+        <location filename="../singleappdialog.cpp" line="117"/>
+        <source>Close</source>
+        <translation type="unfinished">关闭</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="128"/>
         <source> - Update Client</source>
-        <translation type="unfinished">- ¸üÐÂ¿Í»§¶Ë</translation>
+        <translation type="unfinished">- 更新客户端</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="230"/>
         <source>Update &apos;%1&apos; installed successfully</source>
-        <translation type="unfinished">¸üÐÂ &apos;%1&apos; °²×°³É¹¦</translation>
+        <translation type="unfinished">更新 &apos;%1&apos; 安装成功</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="240"/>
         <source>Update &apos;%1&apos; failed with error %2</source>
-        <translation type="unfinished">¸üÐÂ &apos;%1&apos; ·¢Éú´íÎó Ê§°Ü%2</translation>
+        <translation type="unfinished">更新 &apos;%1&apos; 发生错误 失败%2</translation>
     </message>
     <message>
         <location filename="../singleappdialog.cpp" line="248"/>
         <source>Update &apos;%1&apos; closed unexpected</source>
-        <translation type="unfinished">¸üÐÂ&apos;%1&apos;ÒâÍâ¹Ø±Õ</translation>
+        <translation type="unfinished">更新&apos;%1&apos;意外关闭</translation>
     </message>
 </context>
 <context>
@@ -227,12 +246,12 @@
     <message>
         <location filename="../commander.cpp" line="246"/>
         <source>Installing update &apos;%1&apos;</source>
-        <translation type="unfinished">°²×°¸üÐÂ&apos;%1&apos;</translation>
+        <translation type="unfinished">安装更新&apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../commander.cpp" line="328"/>
         <source>Error: Update &apos;%1&apos; failed to start</source>
-        <translation type="unfinished">´íÎó¸üÐÂ&apos;%1&apos; ¿ªÊ¼Ê§°Ü</translation>
+        <translation type="unfinished">错误更新&apos;%1&apos; 开始失败</translation>
     </message>
 </context>
 <context>
@@ -240,7 +259,7 @@
     <message>
         <location filename="../systemtray.cpp" line="72"/>
         <source>Software Update Manager</source>
-        <translation type="unfinished">Èí¼þ¸üÐÂ¹ÜÀíÆ÷</translation>
+        <translation type="unfinished">软件更新管理器</translation>
     </message>
 </context>
 <context>
@@ -248,66 +267,66 @@
     <message>
         <location filename="../usermessages.ui" line="93"/>
         <source>Mark as read</source>
-        <translation type="unfinished">±ê¼ÇÎªÒÑ¶Á</translation>
+        <translation type="unfinished">标记为已读</translation>
     </message>
     <message>
         <location filename="../usermessages.ui" line="135"/>
         <source>Not Now</source>
-        <translation type="unfinished">²»ÊÇÏÖÔÚ</translation>
+        <translation type="unfinished">不是现在</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="104"/>
         <location filename="../usermessages.cpp" line="141"/>
         <source> - Message</source>
-        <translation type="unfinished">-ÐÅÏ¢</translation>
+        <translation type="unfinished">-信息</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="106"/>
         <source> - Messages</source>
-        <translation type="unfinished">- ÐÅÏ¢</translation>
+        <translation type="unfinished">- 信息</translation>
+    </message>
+    <message>
+        <location filename="../usermessages.cpp" line="132"/>
+        <source> (%1/%2)</source>
+        <translation type="unfinished"> (%1/%2)</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="143"/>
         <source>Not now</source>
-        <translation type="unfinished">²»ÊÇÏÖÔÚ</translation>
+        <translation type="unfinished">不是现在</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="144"/>
         <source>There is a new message available:&lt;br&gt;&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;&lt;br&gt;&lt;br&gt;Do you want to read this message in your standard browser now?</source>
-        <translation type="unfinished">ÓÐÒ»ÌõÐÂÏûÏ¢:&lt;br&gt;&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;&lt;br&gt;&lt;br&gt;ÄãÏÖÔÚÊÇ·ñÏëÓÃ±ê×¼ä¯ÀÀÆ÷ÔÄ¶Á´ËÏûÏ¢£¿</translation>
+        <translation type="unfinished">有一条新消息:&lt;br&gt;&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;&lt;br&gt;&lt;br&gt;你现在是否想用标准浏览器阅读此消息？</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="172"/>
         <source>Loading %p% ...</source>
-        <translation type="unfinished">¼ÓÔØ %p% ...</translation>
-    </message>
-    <message>
-        <location filename="../usermessages.cpp" line="188"/>
-        <source>Mark as read and close</source>
-        <translation type="unfinished">±ê¼ÇÎªÒÑ¶Á</translation>
+        <translation type="unfinished">加载 %p% ...</translation>
     </message>
     <message>
         <location filename="../usermessages.cpp" line="188"/>
         <source></source>
-        <translation>±ê¼ÇÎªÒÑ¶Á²¢¹Ø±Õ</translation>
+        <translation type="unfinished">标记为已读并关闭</translation>
     </message>
 </context>
 <context>
     <name>UserMessagesEx</name>
     <message>
         <location filename="../usermessages_ex.ui" line="76"/>
-        <source>Mark as read</source>
-        <translation type="unfinished">±ê¼ÇÎªÒÑ¶Á</translation>
+        <source>Mark as read and close</source>
+        <translation type="unfinished">标记为已读</translation>
     </message>
     <message>
         <location filename="../usermessages_ex.ui" line="118"/>
         <source>Not Now</source>
-        <translation type="unfinished">²»ÊÇÏÖÔÚ</translation>
+        <translation type="unfinished">不是现在</translation>
     </message>
     <message>
         <location filename="../usermessages_ex.ui" line="128"/>
         <source>about:blank</source>
-        <translation type="unfinished">¿Õ°×Ò³</translation>
+        <translation type="unfinished">空白页</translation>
     </message>
 </context>
 <context>
@@ -315,47 +334,47 @@
     <message>
         <location filename="../usernotofication.ui" line="57"/>
         <source>A new software update is available</source>
-        <translation type="unfinished">Ò»¸öÐÂµÄÈí¼þ¸üÐÂ</translation>
+        <translation type="unfinished">一个新的软件更新</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="70"/>
         <source>Do you want to download and install the update now?</source>
-        <translation type="unfinished">ÄãÏëÒªÏÖÔÚÏÂÔØ²¢°²×°´Ë¸üÐÂ?</translation>
+        <translation type="unfinished">你想要现在下载并安装此更新?</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="119"/>
         <source>Name</source>
-        <translation type="unfinished">ÐÕÃû</translation>
+        <translation type="unfinished">姓名</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="124"/>
         <source>Version</source>
-        <translation type="unfinished">°æ±¾</translation>
+        <translation type="unfinished">版本</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="129"/>
         <source>Size</source>
-        <translation type="unfinished">´óÐ¡</translation>
+        <translation type="unfinished">大小</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="165"/>
         <source>Show Details</source>
-        <translation type="unfinished">ÏÔÊ¾ÏêÏ¸ÐÅÏ¢</translation>
+        <translation type="unfinished">显示详细信息</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="188"/>
         <source>Not Now</source>
-        <translation type="unfinished">²»ÊÇÏÖÔÚ</translation>
+        <translation type="unfinished">不是现在</translation>
     </message>
     <message>
         <location filename="../usernotofication.ui" line="198"/>
         <source>Continue</source>
-        <translation type="unfinished">¼ÌÐø</translation>
+        <translation type="unfinished">继续</translation>
     </message>
     <message>
         <location filename="../usernotofication.cpp" line="74"/>
         <source> - Update Client</source>
-        <translation type="unfinished"> - ¸üÐÂ¿Í»§¶Ë</translation>
+        <translation type="unfinished"> - 更新客户端</translation>
     </message>
 </context>
 </TS>
